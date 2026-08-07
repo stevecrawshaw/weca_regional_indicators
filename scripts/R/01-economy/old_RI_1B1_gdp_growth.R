355cdcdc3c30f6c48c728c15d10e31cb2ee65e90
Steve Crawshaw <murrayjarvis@gmail.com>
2026-08-07 18:13:41 +0100

    fix: gdp growth updated

--- scripts/R/01-economy/old_RI_1B1_gdp_growth.R
+++ scripts/R/01-economy/old_RI_1B1_gdp_growth.R
@@ -2,7 +2,7 @@
 pacman::p_load(tidyverse, glue, janitor, here, readxl)
 source(here::here("scripts", "R", "_common.R"))
 
-# Read in cvm data
+# Read in data
 RI_1B1_cvm_raw_tbl <- read_excel(
   here::here(
     "data",
@@ -32,62 +32,28 @@ weca_cvm_gva_data <- RI_1B1_cvm_raw_tbl |>
 # Pivot and clean year column
 weca_cvm_gva_data_clean <- weca_cvm_gva_data |>
   pivot_longer(x2012:x2023, names_to = "year", values_to = "gdp") |>
-  mutate(year = str_remove(year, "x") |> as.integer()) ## |>
-## glimpse()
+  mutate(year = str_remove(year, "x") |> as.integer()) ##|>
+##glimpse()
 
-# Calculate growth rates for 2012 to 2023
-weca_cvm_gva_growth <- weca_cvm_gva_data_clean |>
-  arrange(year) |>
-  mutate(
-    year = as.numeric(sub("x", "", year)),
-    growth_rate = (gdp - lag(gdp)) / lag(gdp) * 100 # Percentage change formula
-  ) |>
-  filter(year >= 2014)
+# Create base year variable
+base <- weca_cvm_gva_data_clean |>
+  filter(year == 2013) |>
+  pull(gdp)
 
-## glimpse(weca_cvm_gva_growth)
-
-# Read in cp data
-RI_1B1_cp_raw <- read_excel(
-  here::here(
-    "data",
-    "raw",
-    "regionalgrossdomesticproductgdplocalauthorities.xlsx"
-  ),
-  sheet = "Table 5",
-  skip = 1
-) ## |> glimpse()
-
-
-# Clean and filter data
-weca_cp_gva_data <- RI_1B1_cp_raw |>
-  clean_names() |>
-  filter(la_name %in% local_authorities) ## |>
-## glimpse()
-
-# Aggregate CP data for 2023
-base_price_2023 <- weca_cp_gva_data |>
-  summarise(cp_2023 = sum(x2023, na.rm = TRUE)) |>
-  pull(cp_2023) ## |>
-## glimpse()
-
-# create CVM series in 2023 prices
-weca_cvm_series <- tibble(
-  year = 2014:2023,
-  growth_rate = weca_cvm_gva_growth$growth_rate
-) |>
-  mutate(
-    factor = cumprod(1 + growth_rate / 100),
-    cvm_value = factor / factor[year == 2023] * base_price_2023
-  )
-
-## glimpse(weca_cvm_series)
+# Create index and growth columns
+weca_gva_index <- weca_cvm_gva_data_clean |>
+  mutate(index = gdp / base * 100) |>
+  mutate(growth = (gdp / lag(gdp, 1) - 1) * 100 |> round(1)) |>
+  filter(year >= 2013) ##|>
+##glimpse()
 
 # Add period_start and period_end as Date objects
-weca_cvm_series <- weca_cvm_series |>
+weca_gva_index <- weca_gva_index |>
   mutate(
     period_start = make_date(year, 1, 1),
     period_end = make_date(year, 12, 31)
-  ) ## |> glimpse()
+  ) ## |>
+##glimpse()
 
 #create upper and lower limits
 #function applies the approach and ceiling rounds x up
@@ -97,8 +63,8 @@ round_to_interval <- function(x, interval = 10) {
 }
 
 #min and max values from data
-min_value <- min(weca_cvm_series$cvm_value, na.rm = FALSE)
-max_value <- max(weca_cvm_series$cvm_value, na.rm = FALSE)
+min_value <- min(weca_gva_index$index, na.rm = FALSE)
+max_value <- max(weca_gva_index$index, na.rm = FALSE)
 
 #lower limit is where floor rounds down the min_value
 #upper limit takes the rounded up upper_limit
@@ -107,37 +73,33 @@ upper_limit <- round_to_interval(max_value, interval = 10)
 
 
 # Generate the plot
-RI_1B1_plot <- ggplot(weca_cvm_series, aes(x = period_start, y = cvm_value)) +
+RI_1B1_plot <- ggplot(weca_gva_index, aes(x = period_start, y = index)) +
   geom_line(linewidth = 1, color = get_weca_color("west_green")) +
   geom_point(size = 2, color = get_weca_color("west_green")) +
   labs(
-    title = "GDP growth in 2023 prices",
+    title = "GDP growth since 2013",
     subtitle = "Real GDP (excluding inflation) for the West of England",
     x = "Year",
-    y = "GDP\n(£m)",
+    y = "GDP\ngrowth\n(%)",
     caption = "Source: Office for National Statistics, subnational GDP"
   ) +
   scale_x_date(
     date_breaks = "1 year", # Corrected to singular "year"
     date_labels = "%Y"
   ) +
-  scale_y_continuous(
-    limits = c(lower_limit, upper_limit),
-    labels = scales::label_dollar(prefix = "£", big.mark = ",")
-  ) +
-
+  scale_y_continuous(limits = c(lower_limit, upper_limit)) +
   theme_weca() +
   theme(
     axis.title.y = element_text(angle = 0, vjust = 0.5) # Rotate y-axis title to horizontal
   )
 
 # Display the plot
-# RI_1B1_plot
+RI_1B1_plot
 
 # Select columns for the table
-RI_1B1_gdp_growth_tbl <- weca_cvm_series |>
-  select(period_start, period_end, value = cvm_value) ## |>
-## glimpse()
+RI_1B1_gdp_growth_tbl <- weca_gva_index |>
+  select(period_start, period_end, value = growth) ##|>
+##glimpse()
 
 # Save fact file
 RI_1B1_gdp_growth_tbl |>
